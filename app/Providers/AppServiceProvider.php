fca6a36cbc7589c5cf53679156d178d9c866c044
Miles Wu <slion0725@gmail.com>
2024-11-24 15:49:07 +0000

    upd

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Model;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // * https://laravel-news.com/shouldbestrict
+        Model::shouldBeStrict();
+
+        // * 在測試環境中強制關閉
+        // * https://laravel.com/docs/11.x/eloquent#configuring-eloquent-strictness
+        // * https://laravel-news.com/disable-eloquent-lazy-loading-during-development
+
+        // * 防止延遲載入
+        // Model::preventLazyLoading(! $this->app->isProduction());
+
+        // * 防止默默地丟棄屬性
+        // Model::preventSilentlyDiscardingAttributes(! $this->app->isProduction());
+
+        // * 防止存取遺失的屬性
+        // Model::preventAccessingMissingAttributes(! $this->app->isProduction());
     }
 }
